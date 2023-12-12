8065a2b8fa3c4aec2bfdd1b3018475fd89c7c97f
Mloche <loche.eu@gmail.com>
2023-12-12 23:14:33 +0000

    we did it, v2

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return {"Hello":"We Love datascientest !"}
+  return {"Hello":"We Love datascientest, adn we dit it ! We built a CI-CD pipeline !!"}
